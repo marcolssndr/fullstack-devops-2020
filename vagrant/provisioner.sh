db24052c0f53f2e47ce53e6c5e96991aec05dc29
marco <alessandramarcoo00@gmail.com>
2022-03-30 14:39:28 +0200

    provisioner modificato

--- vagrant/provisioner.sh
+++ vagrant/provisioner.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-readonly USER_NAME="Gianni Bombelli (bombo82)"
-readonly USER_EMAIL="gianni@giannibombelli.it"
+readonly USER_NAME="Marco Alessandra (marcolssndr)"
+readonly USER_EMAIL="alessandramarcoo00@gmail.com"
 
 install_and_configure_git() {
     echo "### Install and configure git"
